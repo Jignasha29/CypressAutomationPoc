7ee2223cf2df153af39cb0d71cdac9468d594c64
jignasha@umich.edu <jvaghashiya@xtivia.com>
2025-11-27 10:10:47 +0530

    added one more scenario to intercept request

--- cypress/integration/examples/APITest.js
+++ cypress/integration/examples/APITest.js
@@ -22,4 +22,29 @@ describe('Test Suite', function () {
     
   })
 
+  it('2. Modify request URL by adding book name',function(){
+    cy.visit("https://rahulshettyacademy.com/angularAppdemo/"); 
+    cy.intercept({
+      method: 'GET',
+      url: 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
+    },
+    (req) =>{
+       {
+        req.url = req.url + '&book_name=RestAssured with Java'
+        req.continue(); //sends the modified request to the actual serrver
+      }
+    }).as('modifiedRequest');
+    cy.get("button[class='btn btn-primary']").click();
+    cy.wait('@modifiedRequest').then((interception)=>{
+      cy.log('Final URL sent to server:',interception.request.url)
+      expect(interception.request.url).to.include('book_name=RestAssured with Java');
+      expect(interception.response.statusCode).to.equal(200);
+      const responseBody = interception.response.body;
+      expect(responseBody[0]).to.have.property('book_name');
+      expect(responseBody[0]).to.have.property('isbn');
+      expect(responseBody[0]).to.have.property('aisle');
+    })
+  })
+
+
 });
